5b0332883918ababc298fe6edece78eda7156cae
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-05 23:35:45 +0530

    add: added example of struct field in updation

--- struct_playground/src/main.rs
+++ struct_playground/src/main.rs
@@ -13,12 +13,17 @@ fn main() {
     /**
      * Instantiating a struct. For this case, we are instantiating User
      */
-    let user1 = User {
+    let mut user1 = User {
         active: true,
         username: String::from("johndoe"),
         email: String::from("someone@email.com"),
         sign_in_counter: 1,
     };
 
+    // Accessing a struct field
+    println!("{}", user1.username);
+
+    // Updating a field of a struct
+    user1.username = String::from("johndoeanother");
     println!("{}", user1.username);
 }
