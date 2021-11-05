9cf914653838353209b5ff0c61195d8e3c20cbd5
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-05 23:33:11 +0530

    add: added a custom struct and displayer

--- struct_playground/src/main.rs
+++ struct_playground/src/main.rs
@@ -1,3 +1,24 @@
+/**
+ * defining a struct
+ */
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_counter: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    /**
+     * Instantiating a struct. For this case, we are instantiating User
+     */
+    let user1 = User {
+        active: true,
+        username: String::from("johndoe"),
+        email: String::from("someone@email.com"),
+        sign_in_counter: 1,
+    };
+
+    println!("{}", user1.username);
 }
