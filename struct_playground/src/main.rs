d239d630eb193bc0f5b8280ad571f7c695ed5c7a
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-05 23:39:15 +0530

    add: added struct builder utils function

--- struct_playground/src/main.rs
+++ struct_playground/src/main.rs
@@ -9,11 +9,24 @@ struct User {
     sign_in_counter: u64,
 }
 
+/**
+ * creating a function for returning a struct based on the params
+ */
+
+fn build_struct(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_counter: 1,
+    }
+}
+
 fn main() {
     /**
      * Instantiating a struct. For this case, we are instantiating User
      */
-    let mut user1 = User {
+    let mut user_a = User {
         active: true,
         username: String::from("johndoe"),
         email: String::from("someone@email.com"),
@@ -21,9 +34,15 @@ fn main() {
     };
 
     // Accessing a struct field
-    println!("{}", user1.username);
+    println!("{}", user_a.username);
 
     // Updating a field of a struct
-    user1.username = String::from("johndoeanother");
-    println!("{}", user1.username);
+    user_a.username = String::from("johndoeanother");
+    println!("{}", user_a.username);
+
+    let user_b = build_struct(
+        String::from("joemarkberg"),
+        String::from("joemarkberg@email.com"),
+    );
+    println!("{}", user_b.username);
 }
