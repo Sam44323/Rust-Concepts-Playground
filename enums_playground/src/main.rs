e8deb8ed542144a4982813745afd66a9e17010fe
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-06 13:31:55 +0530

    add: added enum initializer example

--- enums_playground/src/main.rs
+++ enums_playground/src/main.rs
@@ -1,3 +1,29 @@
+// example of creating an enum
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn basic_enun_init() {
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+    println!("home: {:?}", home.kind);
+}
+
 fn main() {
-    println!("enums!");
+    basic_enun_init();
 }
