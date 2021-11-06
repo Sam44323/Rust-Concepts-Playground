c9e55c35bd34b76a8bba2f5b5221be832f2112ed
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-06 15:18:15 +0530

    add: added simple example of control flow match

--- control_flow_enum_playground/src/main.rs
+++ control_flow_enum_playground/src/main.rs
@@ -1,3 +1,24 @@
+// Simple match flows example
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 fn main() {
-    println!("control-flow");
+    value_in_cents(Coin::Penny);
 }
