8b82a1338f9a3016a000b03ff7ea8309b2d9d261
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-06 22:18:21 +0530

    add: added functions to module for updating a string

--- collections_common_playground/src/collections/string.rs
+++ collections_common_playground/src/collections/string.rs
@@ -1,5 +1,23 @@
 pub mod Strings {
+  pub fn create_string() -> String {
+    let s = String::from("This is string");
+    /*
+    Alternative ways to create a string are as follows:
+    - String::new();
+    - "(you string name)".to_string();
+    */
+    return s;
+  }
+
+  pub fn updating_strings() {
+    let mut s = create_string();
+    println!("Original String: {}", s);
+    s.push_str(" and this is the another string"); // use push_str() to append a string to a string
+    s.push('!'); // use push() to append a character to a string
+    println!("Updated String: {}", s);
+  }
+
   pub fn method_callers() {
-    println!("nice");
+    updating_strings();
   }
 }
