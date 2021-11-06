cefdb4c9b9eac2b06881b7fbfd38a0405bb819c2
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-06 20:03:43 +0530

    add: added utils messages for libraries

--- packages_and_crates/restaurant/src/lib.rs
+++ packages_and_crates/restaurant/src/lib.rs
@@ -1,16 +1,26 @@
 mod front_of_house {
     pub mod hosting {
-        pub fn add_to_waitlist() {}
+        pub fn add_to_waitlist() {
+            println!("Added to wailist!");
+        }
 
-        fn seat_at_table() {}
+        fn seat_at_table() {
+            println!("Booked a seat at table!");
+        }
     }
 
     mod serving {
-        fn take_order() {}
+        fn take_order() {
+            println!("Order taking in process!");
+        }
 
-        fn serve_order() {}
+        fn serve_order() {
+            println!("Order serving in process!");
+        }
 
-        fn take_payment() {}
+        fn take_payment() {
+            println!("Payment taking in process!");
+        }
     }
 }
 
