bf92cf4084b3ab0ff175e6085cbf99c4f68fc981
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-06 13:38:50 +0530

    add: added embedded enumerization examples

--- enums_playground/src/main.rs
+++ enums_playground/src/main.rs
@@ -22,8 +22,24 @@ fn basic_enun_init() {
         address: String::from("::1"),
     };
     println!("home: {:?}", home.kind);
+    println!("home: {:?}", loopback.kind);
+}
+
+#[derive(Debug)]
+enum IpAddrKindEmbed {
+    V4(String),
+    V6(String),
+}
+
+fn enum_embedded_example() {
+    let home = IpAddrKindEmbed::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrKindEmbed::V6(String::from("::1"));
+
+    println!("home: {:?}", home);
+    println!("home: {:?}", loopback);
 }
 
 fn main() {
     basic_enun_init();
+    enum_embedded_example();
 }
