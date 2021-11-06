5f3f56e9bff7d5400a55ca26909536ed1f968202
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-06 15:27:17 +0530

    add: added example for nested value matching

--- control_flow_enum_playground/src/main.rs
+++ control_flow_enum_playground/src/main.rs
@@ -19,6 +19,41 @@ fn value_in_cents(coin: Coin) -> u8 {
     }
 }
 
+// Nested value matching example
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Florida,
+}
+
+enum CoinNew {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn nested_matching_example(coin: CoinNew) -> Option<u8> {
+    match coin {
+        CoinNew::Penny => {
+            println!("Lucky penny!");
+            return Some(1);
+        }
+        CoinNew::Nickel => Some(5),
+        CoinNew::Dime => Some(10),
+        CoinNew::Quarter(state) => match state {
+            UsState::Alabama => Some(25),
+            UsState::Alaska => Some(50),
+            UsState::Florida => Some(100),
+        },
+    }
+}
 fn main() {
     value_in_cents(Coin::Penny);
+    println!(
+        "{:?}",
+        nested_matching_example(CoinNew::Quarter(UsState::Alabama))
+    );
 }
