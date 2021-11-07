0b020173b8a4b988698e978a9ca9062ae0398694
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-07 21:25:50 +0530

    add: added docs for performance when used generics

--- types_traits_playground/src/generics_types/generics.rs
+++ types_traits_playground/src/generics_types/generics.rs
@@ -1,3 +1,22 @@
+/*
+Performance with generics:
+----------------------------
+You might be wondering whether there is a runtime cost when you’re using generic type parameters. The good news is that Rust implements generics in such a way that your code doesn’t run any slower using generic types than it would with concrete types.
+
+Rust accomplishes this by performing monomorphization of the code that is using generics at compile time. Monomorphization is the process of turning generic code into specific code by filling in the concrete types that are used when compiled.
+
+In this process, the compiler does the opposite of the steps we used to create the generic function in Listing 10-5: the compiler looks at all the places where generic code is called and generates code for the concrete types the generic code is called with.
+
+Let’s look at how this works with an example that uses the standard library’s Option<T> enum:
+
+
+
+let integer = Some(5);
+let float = Some(5.0);
+
+When Rust compiles this code, it performs monomorphization. During that process, the compiler reads the values that have been used in Option<T> instances and identifies two kinds of Option<T>: one is i32 and the other is f64. As such, it expands the generic definition of Option<T> into Option_i32 and Option_f64, thereby replacing the generic definition with the specific ones.
+*/
+
 fn struct_generics_single_type() {
   struct Point<T> {
     x: T,
