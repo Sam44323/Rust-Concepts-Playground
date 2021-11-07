e5ac7aebe14c8b520fe285394e33cf87d8aa92f0
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-07 20:59:26 +0530

    add: added generics for structs

--- types_traits_playground/src/generics_types/generics.rs
+++ types_traits_playground/src/generics_types/generics.rs
@@ -1,3 +1,14 @@
+fn struct_generics() {
+  struct Point<T> {
+    x: T,
+    y: T,
+  }
+  let integer = Point { x: 5, y: 10 };
+  println!("X: {} ; Y: {}", integer.x, integer.y);
+  //let float = Point { x: 1.0, y: 4.0 }; // will give an error because of type mismatch
+}
+
 pub fn generic_method_caller() {
   println!("generic_method_caller");
+  struct_generics();
 }
