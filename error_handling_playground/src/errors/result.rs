73cd016270a256812bbe5e6ab58f7bca44418695
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-07 18:56:58 +0530

    add: added error type matching handlers

--- error_handling_playground/src/errors/result.rs
+++ error_handling_playground/src/errors/result.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 pub fn result_error_handler() {
   let f = File::open("hello.txt");
@@ -9,6 +10,23 @@ pub fn result_error_handler() {
   }
 }
 
+pub fn error_type_handler() {
+  // matching on Different Errors
+
+  let f = File::open("hello.txt");
+
+  match f {
+    Ok(file) => file,
+    Err(error) => match error.kind() {
+      ErrorKind::NotFound => match File::create("hello.txt") {
+        Ok(fc) => fc,
+        Err(e) => panic!("Problem creating the file: {:?}", e),
+      },
+      _ => panic!("Problem opening the file: {:?}", error),
+    },
+  }
+}
 pub fn result_modules_method_caller() {
   result_error_handler();
+  error_type_handler();
 }
