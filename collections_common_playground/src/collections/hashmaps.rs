e54cd87fd303bc819a437e3f15a7ff7b35ee06b7
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-07 13:54:29 +0530

    add: added option for collect hashing

--- collections_common_playground/src/collections/hashmaps.rs
+++ collections_common_playground/src/collections/hashmaps.rs
@@ -10,7 +10,18 @@ pub mod HashMaps {
     scores.insert(String::from("Yellow"), 50);
   }
 
+  pub fn collect_hashing() {
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    // Using the way of constructing a hash map
+    // is by using iterators and the collect method on a vector of tuples,
+    //which is a collection of key-value pairs.
+    let scores: super::HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    println!("{:?}", scores);
+  }
+
   pub fn method_callers() {
-    println!("caller");
+    collect_hashing();
   }
 }
