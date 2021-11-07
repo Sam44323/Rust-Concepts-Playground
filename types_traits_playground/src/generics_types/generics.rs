add6a8fcd468640608eae82281ec0f886f386451
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-07 21:02:13 +0530

    add: added multi struct types function for examples

--- types_traits_playground/src/generics_types/generics.rs
+++ types_traits_playground/src/generics_types/generics.rs
@@ -8,7 +8,20 @@ fn struct_generics_single_type() {
   println!("X: {} ; Y: {}", integer.x, integer.y);
 }
 
+fn struct_generics_multiple_type() {
+  struct Point<T, U> {
+    x: T,
+    y: U,
+  }
+  let multi_type = Point {
+    x: 5,
+    y: String::from("This is another type!"),
+  };
+  println!("X: {} ; Y: {}", multi_type.x, multi_type.y);
+}
+
 pub fn generic_method_caller() {
   println!("generic_method_caller");
   struct_generics_single_type();
+  struct_generics_multiple_type();
 }
