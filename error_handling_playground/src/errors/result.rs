4fa082e997b68d5717f43cf7d62dedc70f6b6ed9
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-07 18:45:58 +0530

    add: added example of error handlers using Result

--- error_handling_playground/src/errors/result.rs
+++ error_handling_playground/src/errors/result.rs
@@ -1,1 +1,14 @@
-pub fn result_modules_method_caller() {}
+use std::fs::File;
+
+pub fn result_error_handler() {
+  let f = File::open("hello.txt");
+
+  match f {
+    Ok(file) => file,
+    Err(error) => panic!("Problem opening the file: {:?}", error),
+  }
+}
+
+pub fn result_modules_method_caller() {
+  result_error_handler();
+}
