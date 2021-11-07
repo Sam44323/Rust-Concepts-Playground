e1d883c3e45be4ab8a7b39b74a1e3aef1e4a38ed
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-07 13:01:43 +0530

    add: added docs for hashmaps

--- collections_common_playground/src/collections/hashmaps.rs
+++ collections_common_playground/src/collections/hashmaps.rs
@@ -1,6 +1,13 @@
 use std::collections::HashMap;
 
 pub mod HashMaps {
+  pub fn create_hashmap() {
+    let mut scores = super::HashMap::new(); // creating a hashmap
+
+    scores.insert(String::from("Blue"), 10); // inserting values to hashmap
+    scores.insert(String::from("Yellow"), 50);
+  }
+
   pub fn method_callers() {
     println!("caller");
   }
