1f61b997b83da35e069f0e70c28a07d1e5f46732
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-08 17:22:10 +0530

    add: added example of converting a function to a test function

--- test_automated_playground/src/main.rs
+++ test_automated_playground/src/main.rs
@@ -1,3 +1,12 @@
+#[cfg(test)]
+/*
+To change a function into a test function, add #[test] on the line before fn. When you run your tests with the cargo test command, Rust builds a test runner binary that runs the functions annotated with the test attribute and reports on whether each test function passes or fails.
+*/
+#[test]
+fn it_works() {
+    assert_eq!(2 + 2, 4);
+}
+
 fn main() {
     println!("test!");
 }
