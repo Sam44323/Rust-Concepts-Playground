d388600c13a8313b923096d1bd2e8738f5321fe9
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-08 17:38:10 +0530

    add: added examples for testing using the assert! macro

--- test_automated_playground/src/main.rs
+++ test_automated_playground/src/main.rs
@@ -3,8 +3,45 @@
 To change a function into a test function, add #[test] on the line before fn. When you run your tests with the cargo test command, Rust builds a test runner binary that runs the functions annotated with the test attribute and reports on whether each test function passes or fails.
 */
 #[test]
-fn it_works() {
+fn exploration() {
     assert_eq!(2 + 2, 4);
+}
+
+// example of using the assert! macro
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+mod tests {
+
+    /*
+    Note:
+    Because the tests module is an inner module, we need to bring the code under test in the outer module into the scope of the inner module. We use a glob here so anything we define in the outer module is available to this tests module.
+    */
+
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
 }
 
 fn main() {
