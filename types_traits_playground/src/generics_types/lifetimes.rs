7604b6eba723c6e4043d303232e5e6c27a123f5f
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-15 21:55:45 +0530

    add: added structs implementations for own

--- types_traits_playground/src/generics_types/lifetimes.rs
+++ types_traits_playground/src/generics_types/lifetimes.rs
@@ -11,6 +11,21 @@ fn single_lifetimes<'a>(x: &'a str, y: &str) -> &'a str {
   x
 }
 
+// example of using structs with lifetimes
+
+struct ImportantExcerpt<'a> {
+  part: &'a str,
+}
+
+fn struct_lifetimes() {
+  let novel = String::from("Call me Ishmael. Some years ago...");
+  let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+  let i = ImportantExcerpt {
+    part: first_sentence,
+  };
+  println!("Structs using lifetimes: {}", i.part);
+}
+
 fn longest_lifetimes() {
   let string1 = String::from("longest");
   let string2 = String::from("short");
@@ -20,6 +35,7 @@ fn longest_lifetimes() {
   let another_result = single_lifetimes(&string1, &string2);
   println!("The static string renderer is:  {}", another_result);
   println!("---------------------------------");
+  struct_lifetimes();
 }
 
 pub fn lifetimes_method_caller() {
