8160fa12a8098c317c52166c59d4c2bdb700eb8f
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-15 21:48:55 +0530

    add: added single static render method

--- types_traits_playground/src/generics_types/lifetimes.rs
+++ types_traits_playground/src/generics_types/lifetimes.rs
@@ -6,11 +6,20 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
   }
 }
 
+// example single lifetime
+fn single_lifetimes<'a>(x: &'a str, y: &str) -> &'a str {
+  x
+}
+
 fn longest_lifetimes() {
   let string1 = String::from("longest");
   let string2 = String::from("short");
   let result = longest(&string1, &string2);
   println!("The longest string is {}", result);
+  println!("---------------------------------");
+  let another_result = single_lifetimes(&string1, &string2);
+  println!("The static string renderer is:  {}", another_result);
+  println!("---------------------------------");
 }
 
 pub fn lifetimes_method_caller() {
