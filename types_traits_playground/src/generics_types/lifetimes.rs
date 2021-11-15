11f2a9665927833eed24e0c939de46dbee75f1e4
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-15 21:22:38 +0530

    add: added example of longest

--- types_traits_playground/src/generics_types/lifetimes.rs
+++ types_traits_playground/src/generics_types/lifetimes.rs
@@ -1,3 +1,18 @@
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+  if x.len() > y.len() {
+    x
+  } else {
+    y
+  }
+}
+
+fn longest_lifetimes() {
+  let string1 = String::from("longest");
+  let string2 = String::from("short");
+  let result = longest(&string1, &string2);
+  println!("The longest string is {}", result);
+}
+
 pub fn lifetimes_method_caller() {
-  println!("lifetimes_method_caller");
+  longest_lifetimes();
 }
