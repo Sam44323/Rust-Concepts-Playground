69813d9674a5c3859aedb0f5b2830175ceb421ee
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-15 22:26:04 +0530

    add: added std::env:: implmentations for the command line parameters data

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
+/*
+use of std::env::args
+-----------------------
+To enable minigrep to read the values of command line arguments we pass to it, we’ll need a function provided in Rust’s standard library, which is std::env::args. This function returns an iterator of the command line arguments that were given to minigrep.
+
+Note: regarding iterators:
+-------------------------
+Iterators produce a series of values, and we can call the collect method on an iterator to turn it into a collection, such as a vector, containing all the elements the iterator produces.
+*/
+
 fn main() {
-    println!("Grep!");
+    let cli_args: Vec<String> = env::args().collect();
+    println!("{:?}", cli_args);
 }
