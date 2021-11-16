ac2e6dd8224bbac868169345601e1488bee67436
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-16 21:52:05 +0530

    add: added struct for config for the filename for reading

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -11,22 +11,33 @@ Note: regarding iterators:
 Iterators produce a series of values, and we can call the collect method on an iterator to turn it into a collection, such as a vector, containing all the elements the iterator produces.
 */
 
-fn parse_config(args: &[String]) -> (&str, &str) {
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
     /*
     Note:
     ------
     As we saw when we printed the vector, the program’s name takes up the first value in the vector at args[0], so we’re starting at index 1.
     */
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+
+    // This will make a full copy of the data for the Config instance to own
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config {
+        query: query,
+        filename: filename,
+    }
 }
 
 fn main() {
     let cli_args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&cli_args);
+    let config = parse_config(&cli_args);
     // reading the content for the files
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    println!("Searching for {} in {}", query, filename);
+    let contents =
+        fs::read_to_string(&config.filename).expect("Something went wrong reading the file");
+    println!("Searching for {} in {}", config.query, config.filename);
     println!("With text:\n{}", contents);
 }
