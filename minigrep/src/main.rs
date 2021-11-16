22e066616ab19293845c3992699924e6e3b66938
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-16 21:58:15 +0530

    update: updated the parser with implementation for the config struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -16,25 +16,25 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    /*
-    Note:
-    ------
-    As we saw when we printed the vector, the program’s name takes up the first value in the vector at args[0], so we’re starting at index 1.
-    */
+impl Config {
+    fn new(args: &[String]) -> Self {
+        /*
+        Note:
+        ------
+        As we saw when we printed the vector, the program’s name takes up the first value in the vector at args[0], so we’re starting at index 1.
+        */
 
-    // This will make a full copy of the data for the Config instance to own
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config {
-        query: query,
-        filename: filename,
+        // This will make a full copy of the data for the Config instance to own
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
     }
 }
 
 fn main() {
     let cli_args: Vec<String> = env::args().collect();
-    let config = parse_config(&cli_args);
+    let config = Config::new(&cli_args);
     // reading the content for the files
     let contents =
         fs::read_to_string(&config.filename).expect("Something went wrong reading the file");
