44e5dc315c5c02bcf6fd54bd32197572edeafc9f
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-18 21:25:31 +0530

    add: added initial workout generator function for closures

--- functional-lang-features/closures/src/main.rs
+++ functional-lang-features/closures/src/main.rs
@@ -1,3 +1,37 @@
+use std::thread;
+use std::time::Duration;
+
+fn calculation_simualated_async(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            calculation_simualated_async(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            calculation_simualated_async(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                calculation_simualated_async(intensity)
+            );
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
 }
