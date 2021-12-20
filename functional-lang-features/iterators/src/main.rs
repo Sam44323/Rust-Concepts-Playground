bece15722a9faef18ce8e9497c808167c23a76f6
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-20 23:33:01 +0530

    add: added example of consuming iterators using next

--- functional-lang-features/iterators/src/main.rs
+++ functional-lang-features/iterators/src/main.rs
@@ -35,9 +35,24 @@ fn iterator_deepdowns() {
     assert_eq!(v1_iter.next(), None);
 }
 
+/**
+ * Methods that call next are called consuming adaptors, because calling them uses up the iterator. One example is the sum method, which takes ownership of the iterator and iterates through the items by repeatedly calling next, thus consuming the iterator. As it iterates through, it adds each item to a running total and returns the total when iteration is complete.
+ */
+
+fn consume_iterator_examples() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    let total: i32 = v1_iter.sum();
+
+    assert_eq!(total, 6);
+}
+
 fn main() {
     iterator_example();
     iterator_trait_implementation();
     iterator_deepdowns();
+    consume_iterator_examples();
     println!("Hello, world!");
 }
