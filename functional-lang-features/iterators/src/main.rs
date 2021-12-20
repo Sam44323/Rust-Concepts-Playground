dc3712ccf29fde7ec9761b1c1710501d8bca9427
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-20 22:57:36 +0530

    add: added example of iteratore

--- functional-lang-features/iterators/src/main.rs
+++ functional-lang-features/iterators/src/main.rs
@@ -1,3 +1,12 @@
+fn iterator_example() {
+    let v1 = vec![1, 2, 3];
+
+    for val in v1 {
+        println!("Got: {}", val);
+    }
+}
+
 fn main() {
+    iterator_example();
     println!("Hello, world!");
 }
