d0ffa4100f44e03cca1433217a55366c5ad0946f
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-10 22:45:55 +0530

    add: added support for Rc instead of BOx

--- smart_pointers_playground/reference_counter_pointers/src/main.rs
+++ smart_pointers_playground/reference_counter_pointers/src/main.rs
@@ -1,16 +1,22 @@
+use std::rc::Rc;
+
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
+/**
+ * Using the Rc instead of Box
+ * Instead, we’ll change our definition of List to use Rc<T> in place of Box<T>, as shown in Listing 15-18. Each Cons variant will now hold a value and an Rc<T> pointing to a List. When we create b, instead of taking ownership of a, we’ll clone the Rc<List> that a is holding, thereby increasing the number of references from one to two and letting a and b share ownership of the data in that Rc<List>. We’ll also clone a when creating c, increasing the number of references from two to three. Every time we call Rc::clone, the reference count to the data within the Rc<List> will increase, and the data won’t be cleaned up unless there are zero references to it.
+ */
 use crate::List::{Cons, Nil};
 
-fn main() {
-    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
-    let b = Cons(3, Box::new(a));
-    let c = Cons(4, Box::new(a));
+fn list_runner() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }
 
 fn main() {
-    println!("Hello, world!");
+    list_runner();
 }
