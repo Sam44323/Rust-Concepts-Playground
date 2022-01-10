3afea0d3d419bf6cd30c4ccb378331505050c518
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-10 15:56:54 +0530

    add: added dereferencing vs box implementation

---
+++ smart_pointers_playground/deref_and_drops/src/main.rs
@@ -0,0 +1,27 @@
+/**
+- "*" is dereferencing that is getting the value that is being references using "&"
+* Diff between dereferencing_runner and box_dereferencing_runner:
+*
+* The only difference is that here we set y to be an instance of a box pointing to a copied value of x rather than a reference pointing to the value of x. In the box_dereferencing_runner assertion, we can use the dereference operator to follow the box’s pointer in the same way that we did when y was a reference. Next, we’ll explore what is special about Box<T> that enables us to use the dereference operator by defining our own box type.
+*/
+
+fn dereferencing_runner() {
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+fn box_dereferencing_runner() {
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+fn main() {
+    dereferencing_runner();
+    box_dereferencing_runner();
+}
