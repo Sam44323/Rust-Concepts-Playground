e64de39bac7894f4640d8047b6ce550e792700bf
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-24 01:33:11 +0530

    add: added method initializer for the cli

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut guess: String = String::new();
+
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    io::stdin()
+        .read_line(&mut guess) // adding the buffer_data to the guess variable
+        .expect("Failed to read line");
 }
