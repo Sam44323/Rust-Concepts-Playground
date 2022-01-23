24999503ccb041185900ab04220e5ded7f7cca71
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-24 01:41:09 +0530

    add: added random number calculator for the secret_number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,9 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     let mut guess: String = String::new();
+    let secret_number = rand::thread_rng().gen_range(1, 101); // high is exclusive
 
     println!("Guess the number!");
     println!("Please input your guess.");
