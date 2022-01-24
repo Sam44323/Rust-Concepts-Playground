f5dfd5c53f3714f5e710452bfe6ca72040c60080
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-24 22:48:57 +0530

    add: added custom User struct implementation with examples for signing

--- sign_in_counter/src/main.rs
+++ sign_in_counter/src/main.rs
@@ -1,3 +1,25 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut user: User = User {
+        username: String::from("someone"),
+        email: String::from("peakyblinder@email.com"),
+        sign_in_count: 1,
+        active: true,
+    };
+
+    user.username = String::from("Cillian");
+
+    // inheriting some data from another user for an user
+
+    let anotherUser: User = User {
+        username: String::from("anotherUser"),
+        email: String::from("anotherUser@email.com"),
+        ..user
+    };
 }
