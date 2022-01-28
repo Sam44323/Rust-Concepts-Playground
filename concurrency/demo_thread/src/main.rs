477d0f859376cf0c94c2064a5b25efea23ea25ae
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-28 23:18:31 +0530

    add: added handler joiner for threading

--- concurrency/demo_thread/src/main.rs
+++ concurrency/demo_thread/src/main.rs
@@ -1,7 +1,7 @@
 use std::{thread, time::Duration};
 
 fn main() {
-    thread::spawn(|| {
+    let handler = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
@@ -9,7 +9,9 @@ fn main() {
     });
 
     for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
+        println!("hi number {} from the main  thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    handler.join().unwrap(); // even when the main thread get's over, the function will still run until the spawned thread is done
 }
