6cf541201c0d521b02046d50c22e8d08ed4f0b00
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 00:52:43 +0530

    add: addded Arc counter for data

--- concurrency/shared_state/src/main.rs
+++ concurrency/shared_state/src/main.rs
@@ -1,14 +1,29 @@
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() {
     /*
      * Mutex uses locking system to ensure that only one thread can access the data at a time. When accessing, it locks the data during tapping and when done, it unlocks the data.
      */
 
-    let m = Mutex::new(5);
+    // Arc allows you to safely transfer ownership of data between threads
 
-    {
-        let mut num = m.lock().unwrap(); // as num contains MutexGuard, it points to data for the duration of the block using Deref
-        *num = 6;
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 1..11 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+
+        handles.push(handle);
     }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Value for counter: {}", *counter.lock().unwrap());
 }
