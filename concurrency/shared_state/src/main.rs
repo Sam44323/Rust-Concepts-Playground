32bf6ca2f6c10977375f0ae1500ef01590db5105
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 00:42:11 +0530

    add: added mutex_initiator

--- concurrency/shared_state/src/main.rs
+++ concurrency/shared_state/src/main.rs
@@ -1,3 +1,9 @@
+use std::sync::Mutex;
+
 fn main() {
-    println!("Hello, world!");
+    /*
+     * Mutex uses locking system to ensure that only one thread can access the data at a time. When accessing, it locks the data during tapping and when done, it unlocks the data.
+     */
+
+    let m = Mutex::new(5);
 }
