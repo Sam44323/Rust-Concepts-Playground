c51b6e8095543a2de6b3a8b5f0139fdc2ef050d7
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 14:29:58 +0530

    add: added custom select box struct with draw

--- oop/polymorphism_with_trait_object/src/main.rs
+++ oop/polymorphism_with_trait_object/src/main.rs
@@ -1,3 +1,17 @@
+use polymorphism_with_trait_object::*;
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // draw the select box
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
