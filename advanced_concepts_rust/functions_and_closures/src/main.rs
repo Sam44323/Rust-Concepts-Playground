f525df7af8a064b1fd0c798f18fc3507be03c8ca
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 23:09:16 +0530

    add: added example of function args pointers

--- advanced_concepts_rust/functions_and_closures/src/main.rs
+++ advanced_concepts_rust/functions_and_closures/src/main.rs
@@ -1,3 +1,19 @@
+// example of passing functions as arguments to another function
+
+fn function_pointers() {
+    fn add_one(x: i32) -> i32 {
+        x + 1
+    }
+
+    fn do_thrice(f: fn(i32) -> i32, arg: i32) -> i32 {
+        f(arg) + f(arg) + f(arg)
+    }
+
+    let answer = do_thrice(add_one, 5);
+    println!("The answer is {}", answer);
+}
+
 fn main() {
     println!("Hello, world!");
+    function_pointers();
 }
