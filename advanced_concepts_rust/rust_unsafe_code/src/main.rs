16aecc79ebf52d248b7fd70b5c1b649aa1737ab5
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 21:14:17 +0530

    add: added docs for raw_pointers

--- advanced_concepts_rust/rust_unsafe_code/src/main.rs
+++ advanced_concepts_rust/rust_unsafe_code/src/main.rs
@@ -1,3 +1,22 @@
+// unsafe ability 1
+fn dereference_raw_pointers() {
+    let mut num = 5;
+
+    // here * is not dereference but it's simply how we declare raw pointers in rust
+    /*
+     * Properties of raw pointers are as follows:
+     * 1. They are also not guaranteed yo point to point to valid data or memory
+     *
+     * 2. They are allowed to ignore rust borrowing rules by having mutable and immutable pointers or multiple immutable pointers to the same location
+     *
+     * 3. They are allowed to be null
+     *
+     * 4. They don't implement automatic cleanup which mean you can have dangling pointers
+     */
+    let a = &num as *const i32; // immutable raw pointer example is *const <type point to>
+    let b = &mut num as *mut i32; // immutable raw pointer example is *mut <type point to>
+}
+
 fn main() {
-    println!("Hello, world!");
+    dereference_raw_pointers();
 }
