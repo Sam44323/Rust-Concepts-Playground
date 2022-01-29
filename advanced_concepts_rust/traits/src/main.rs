ceddd8b9e2349c8724ee2905c00744e4e427e4d8
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 22:00:34 +0530

    add: added associated types implementation for traits

--- advanced_concepts_rust/traits/src/main.rs
+++ advanced_concepts_rust/traits/src/main.rs
@@ -1,3 +1,15 @@
+// associated types
+// using the associated types in a trait, you can create a type with a specific name that is a member of the trait and then use that type in the trait implementation
+
+/**
+ * Difference between generics and associated types is that with associated types we can have only one concrete type per trait, whereas with generics we can have multiple concrete types per trait.
+ */
+
+pub trait Iterator {
+    type Item;
+    fn next(&mut self) -> Option<Self::Item>;
+}
+
 fn main() {
     println!("Hello, world!");
 }
