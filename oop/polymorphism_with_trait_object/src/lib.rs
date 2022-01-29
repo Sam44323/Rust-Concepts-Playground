0612ac784bf7aa00b48551bcefdb1d1b1c7b8033
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 14:06:57 +0530

    add: added implementation for the Screen_struct

--- oop/polymorphism_with_trait_object/src/lib.rs
+++ oop/polymorphism_with_trait_object/src/lib.rs
@@ -5,3 +5,11 @@ pub trait Draw {
 pub struct Screen {
   pub components: Vec<Box<dyn Draw>>, // dyn Draw means our Box pointer will contain any type that contains the Draw trait
 }
+
+impl Screen {
+  pub fn run(&self) {
+    for component in self.components.iter() {
+      component.draw();
+    }
+  }
+}
