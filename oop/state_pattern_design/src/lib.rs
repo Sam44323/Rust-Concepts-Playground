72aab283ba5d771377f5613559c9bac6f80adb52
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 15:23:21 +0530

    add:a dded request_review method implementation for state

--- oop/state_pattern_design/src/lib.rs
+++ oop/state_pattern_design/src/lib.rs
@@ -1,8 +1,22 @@
-trait State {}
+trait State {
+  fn request_review(self: Box<Self>) -> Box<dyn State>;
+}
 
 struct Draft {}
 
-impl State for Draft {}
+impl State for Draft {
+  fn request_review(self: Box<Self>) -> Box<dyn State> {
+    return Box::new(PendingReview {});
+  }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+  fn request_review(self: Box<Self>) -> Box<dyn State> {
+    self
+  }
+}
 
 pub struct Post {
   state: Option<Box<dyn State>>,
@@ -17,13 +31,17 @@ impl Post {
     }
   }
 
-  pub fn content(&self) -> &str {
-    &self.content
+  pub fn add_text(&mut self, text: &str) {
+    self.content.push_str(text);
   }
 
-  pub fn add_text(&self, text: &str) {}
+  pub fn content(&self) -> &str {
+    ""
+  }
 
-  pub fn request_review(&self) {}
+  pub fn request_review(&mut self) {
+    self.state = Some(Box::new(PendingReview {}));
+  }
 
   pub fn approve(&self) {}
 }
