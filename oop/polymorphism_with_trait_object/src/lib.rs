b60bbaecbf1c3ac824f2dee404236bb1ffe2554c
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 14:22:41 +0530

    update: updated the docs for generics

--- oop/polymorphism_with_trait_object/src/lib.rs
+++ oop/polymorphism_with_trait_object/src/lib.rs
@@ -2,6 +2,13 @@ pub trait Draw {
   fn draw(&self);
 }
 
+/*
+* Here as the list of components can contain any type of component, that implements the Draw trait, if we would've used a generic, then we would've had the option to store only one type of component(such as buttons, slider or etc...), but not a mixture of those. So we are using dyn pointers instead of generic like,
+
+pub struct Screen<T: Draw>{}
+
+*/
+
 pub struct Screen {
   pub components: Vec<Box<dyn Draw>>, // dyn Draw means our Box pointer will contain any type that contains the Draw trait
 }
