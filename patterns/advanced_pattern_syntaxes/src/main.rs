dabacdc98dded3fe61776ac97d6eb5e30309f682
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 19:58:11 +0530

    add: added enum destrcuture matcher with data

--- patterns/advanced_pattern_syntaxes/src/main.rs
+++ patterns/advanced_pattern_syntaxes/src/main.rs
@@ -3,6 +3,13 @@ struct Point {
     y: i32,
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 fn destructure_pattern_matching() {
     let p = Point { x: 0, y: 7 };
 
@@ -20,6 +27,19 @@ fn destructure_pattern_matching() {
         Point { x, y: 0 } => println!("On the x axis at {}", x),
         Point { x: 0, y } => println!("On the y axis at {}", y),
         Point { x, y } => println!("On neither axis at ({}, {})", x, y),
+    }
+
+    // Enums //
+
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => println!("The Quit variant has no data to destructure."),
+        Message::Move { x, y } => println!("x: {} y: {}", x, y),
+        Message::Write(text) => println!("Text: {}", text),
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red: {}, green: {}, blue: {}", r, g, b)
+        }
     }
 }
 
