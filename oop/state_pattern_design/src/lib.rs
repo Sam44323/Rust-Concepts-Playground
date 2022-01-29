66fd81e03f4c87062c3a0813dc39f3b8c9804785
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 16:28:49 +0530

    update: updated the post implementation with draft and actual

--- oop/state_pattern_design/src/lib.rs
+++ oop/state_pattern_design/src/lib.rs
@@ -1,93 +1,25 @@
-trait State {
-  fn request_review(self: Box<Self>) -> Box<dyn State>;
-  fn approve(self: Box<Self>) -> Box<dyn State>;
-  fn content<'a>(&self, post: &'a Post) -> &'a str {
-    ""
-  }
-}
-
-struct Draft {}
-
-impl State for Draft {
-  fn request_review(self: Box<Self>) -> Box<dyn State> {
-    return Box::new(PendingReview {});
-  }
-
-  fn approve(self: Box<Self>) -> Box<dyn State> {
-    self
-  }
-
-  fn content<'a>(&self, post: &'a Post) -> &'a str {
-    ""
-  }
-}
-
-struct PendingReview {}
-
-impl State for PendingReview {
-  fn request_review(self: Box<Self>) -> Box<dyn State> {
-    self
-  }
-
-  fn approve(self: Box<Self>) -> Box<dyn State> {
-    return Box::new(Published {});
-  }
-
-  fn content<'a>(&self, post: &'a Post) -> &'a str {
-    ""
-  }
-}
-
-struct Published {}
-
-impl State for Published {
-  fn request_review(self: Box<Self>) -> Box<dyn State> {
-    self
-  }
-
-  fn approve(self: Box<Self>) -> Box<dyn State> {
-    self
-  }
-
-  fn content<'a>(&self, post: &'a Post) -> &'a str {
-    &post.content
-  }
+pub struct DraftPost {
+  content: String,
 }
 
 pub struct Post {
-  state: Option<Box<dyn State>>,
   content: String,
 }
 
 impl Post {
-  pub fn new() -> Post {
-    Post {
-      state: Some(Box::new(Draft {})),
+  pub fn new() -> DraftPost {
+    DraftPost {
       content: String::new(),
     }
   }
 
-  pub fn add_text(&mut self, text: &str) {
-    self.content.push_str(text);
-  }
-
   pub fn content(&self) -> &str {
-    /*
-     *  as state is an option with boxed state that owns the state object, so we use as_ref to get the reference to the state object along with that we need to unwrap it to get the state
-     */
-    self.state.as_ref().unwrap().content(self)
+    &self.content
   }
+}
 
-  pub fn request_review(&mut self) {
-    // take method takes the inner value out of the Option and places none so that when we call the request_review method we can call the method with the state and get the output that is desired
-    if let Some(state) = self.state.take() {
-      self.state = Some(state.request_review());
-    }
-  }
-
-  pub fn approve(&mut self) {
-    if let Some(state) = self.state.take() {
-      self.state = Some(state.approve());
-    }
+impl DraftPost {
+  pub fn add_text(&mut self, text: &str) {
+    self.content.push_str(text)
   }
 }
--- oop/state_pattern_design/src/main.rs
+++ oop/state_pattern_design/src/main.rs
@@ -1,14 +1,5 @@
 use state_pattern_design::*;
 
 fn main() {
-    let mut post = Post::new();
-
-    post.add_text("This is the first blog!");
-    assert_eq!("", post.content());
-
-    post.request_review();
-    assert_eq!("", post.content());
-
-    post.approve();
-    assert_eq!("This is the first blog!", post.content());
+    println!("nice");
 }
