fb43f1b0d0dc0b7d22e57f1a86e33caf1b55d640
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 15:05:33 +0530

    add: added initial post initializer

--- oop/state_pattern_design/src/main.rs
+++ oop/state_pattern_design/src/main.rs
@@ -1,3 +1,14 @@
+use state_pattern_design::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut post = Post::new();
+
+    post.add_text("This is the first blog!");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("This is the first blog!", post.content());
 }
