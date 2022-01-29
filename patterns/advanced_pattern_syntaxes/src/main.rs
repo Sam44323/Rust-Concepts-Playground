1023859b1d616140e29874671d55ce19200e9bf6
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 20:14:56 +0530

    add: added multiple value matcher with data

--- patterns/advanced_pattern_syntaxes/src/main.rs
+++ patterns/advanced_pattern_syntaxes/src/main.rs
@@ -59,7 +59,19 @@ fn destructure_pattern_matching() {
     // Value ignoring in patterns
     // ------------------------------------------------
 
-    foo(10, 30)
+    foo(10, 30);
+
+    // ------------------------------------------------
+    // Combination pattern matching
+    // ------------------------------------------------
+
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => println!("Can't overwrite an existing customized value"),
+        _ => setting_value = new_setting_value,
+    }
 }
 
 fn foo(_: i32, y: i32) {
