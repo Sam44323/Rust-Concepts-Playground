902b075bc35da8626ccae854f7a2749755750239
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 19:11:09 +0530

    add: added pattern matching initiater with match_expressions

--- patterns/pattern_synt_matching/src/main.rs
+++ patterns/pattern_synt_matching/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    // using the Match Expressions
+    enum Language {
+        English,
+        Spanish,
+        Russian,
+        Japanese,
+    }
+
+    let language = Language::English;
+
+    match language {
+        Language::English => println!("English"),
+        Language::Spanish => println!("Spanish"),
+        Language::Russian => println!("Russian"),
+        _ => println!("Japanese"),
+    }
 }
