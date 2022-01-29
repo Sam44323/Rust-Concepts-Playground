abfceaaaf12ac8ece5cd8b0c61e6fd244ec342aa
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 19:18:54 +0530

    add: added if-let pattern matcher with admin

--- patterns/pattern_synt_matching/src/main.rs
+++ patterns/pattern_synt_matching/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // ------------------------------------------------
     // using the Match Expressions
     // ------------------------------------------------
     #[derive(Debug)]
@@ -19,4 +20,22 @@ fn main() {
     }
 
     // ------------------------------------------------
+    // conditional if-let expressions
+    // ------------------------------------------------
+
+    let authorization_status: Option<&str> = None;
+    let is_admin = false;
+    let group_id: Result<u8, _> = "38".parse();
+
+    if let Some(auth) = authorization_status {
+        println!("Authorization status: {}", auth);
+    } else if is_admin {
+        println!("Admin access for user");
+    } else if let Ok(group) = group_id {
+        if group > 30 {
+            println!("Group id is greater than 30");
+        } else {
+            println!("Group id is less than 30");
+        }
+    }
 }
