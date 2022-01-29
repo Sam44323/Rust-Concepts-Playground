bc2aec67bd8cfbab1898c5e420f446ad13ca8157
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 16:42:05 +0530

    add: added state struct pattern updater for posts

--- oop/state_pattern_design/src/main.rs
+++ oop/state_pattern_design/src/main.rs
@@ -1,5 +1,9 @@
 use state_pattern_design::*;
 
 fn main() {
-    println!("nice");
+    let mut draft_post = Post::new();
+    draft_post.add_text("This is a draft text!");
+    let pending_review_post = draft_post.request_review();
+    let post = pending_review_post.approve();
+    println!("Content for the post: {}", post.content());
 }
