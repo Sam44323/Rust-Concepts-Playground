cbf0271ba31ced25a1612cb669d17bb552288372
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 23:14:56 +0530

    add: added closure returner data

--- advanced_concepts_rust/functions_and_closures/src/main.rs
+++ advanced_concepts_rust/functions_and_closures/src/main.rs
@@ -13,7 +13,15 @@ fn function_pointers() {
     println!("The answer is {}", answer);
 }
 
+fn closure_returning() {
+    // this means this function will return anything that implements the Fn trait that takes an i32 and returns an i32
+    fn returns_closure() -> impl Fn(i32) -> i32 {
+        |x| x + 1
+    }
+}
+
 fn main() {
     println!("Hello, world!");
     function_pointers();
+    closure_returning();
 }
