d460d527779f85711b8210ca0682d0889cd48bd3
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 22:59:03 +0530

    add: added type aliases

--- advanced_concepts_rust/types/src/main.rs
+++ advanced_concepts_rust/types/src/main.rs
@@ -1,3 +1,12 @@
+fn type_aliases() {
+    type Kilometers = i32;
+
+    let x: i32 = 5;
+    let y: Kilometers = 10;
+
+    println!("x is {} and y is {}", x, y);
+}
+
 fn main() {
-    println!("Hello, world!");
+    type_aliases();
 }
