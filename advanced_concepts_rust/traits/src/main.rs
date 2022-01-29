170a4314a5dc18244d33fdca5ad29279d0e5c49c
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 22:06:09 +0530

    add: added scoping for associated_types

--- advanced_concepts_rust/traits/src/main.rs
+++ advanced_concepts_rust/traits/src/main.rs
@@ -1,24 +1,27 @@
-// associated types
-// using the associated types in a trait, you can create a type with a specific name that is a member of the trait and then use that type in the trait implementation
+fn associated_types_examples() {
+    // associated types
+    // using the associated types in a trait, you can create a type with a specific name that is a member of the trait and then use that type in the trait implementation
 
-/**
- * Difference between generics and associated types is that with associated types we can have only one concrete type per trait, whereas with generics we can have multiple concrete types per trait.
- */
+    /*
+     * Difference between generics and associated types is that with associated types we can have only one concrete type per trait, whereas with generics we can have multiple concrete types per trait.
+     */
 
-pub trait Iterator {
-    type Item;
-    fn next(&mut self) -> Option<Self::Item>;
-}
+    pub trait Iterator {
+        type Item;
+        fn next(&mut self) -> Option<Self::Item>;
+    }
 
-struct Counter {}
+    struct Counter {}
 
-impl Iterator for Counter {
-    type Item = i32;
-    fn next(&mut self) -> Option<Self::Item> {
-        Some(1)
+    impl Iterator for Counter {
+        type Item = i32;
+        fn next(&mut self) -> Option<Self::Item> {
+            Some(1)
+        }
     }
 }
 
 fn main() {
     println!("Hello, world!");
+    associated_types_examples();
 }
