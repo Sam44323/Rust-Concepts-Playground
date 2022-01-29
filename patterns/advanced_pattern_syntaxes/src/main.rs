8fa2beb86560bf197990c6cd061c04d6bca506b7
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-29 19:43:55 +0530

    add: added literal matcher with names

--- patterns/advanced_pattern_syntaxes/src/main.rs
+++ patterns/advanced_pattern_syntaxes/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    // ------------------------------------------------
+    // Matching the literals
+    // ------------------------------------------------
+
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    // ------------------------------------------------
+    // Named variables matching with printing
+    // ------------------------------------------------
+
+    let x = Some(10);
+
+    match x {
+        Some(10) => println!("ten"),
+        Some(i) => println!("Value {}", i),
+        None => println!("none"),
+    }
 }
