a66ea683a2499f41fcf88a55f51a342aca88551e
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-30 15:40:09 +0530

    add: added a custom 404 page

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -4,18 +4,22 @@ use std::net::{TcpListener, TcpStream};
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024]; // buffer to store the data from the stream
+    let get = b"GET / HTTP/1.1\r\n"; // the request we are looking for(converting to bytes for checking)
 
-    stream.read(&mut buffer).unwrap(); // sending the data from the stream to the buffer
+    if buffer.starts_with(get) {
+        stream.read(&mut buffer).unwrap(); // sending the data from the stream to the buffer
 
-    let content = fs::read_to_string("index.html").unwrap(); // reading the file
+        let content = fs::read_to_string("index.html").unwrap(); // reading the file
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        content.len(),
-        content
-    );
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap(); // flush will send the data to the client when the entire write process is completed
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            content.len(),
+            content
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap(); // flush will send the data to the client when the entire write process is completed
+    } else {
+    }
 }
 
 fn main() {
