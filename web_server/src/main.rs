b2cb7595c31474e59203de295497897b1486029a
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-30 13:48:21 +0530

    add: added initial server initializer with the stream

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,3 +1,12 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    // incoming gives an iterator over the connections being received in the form of a TCP-Stream
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection is established!");
+    }
 }
