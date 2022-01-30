8211211dcf7364728cf27006d1a8509dd7ef5b2a
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-30 16:23:25 +0530

    add: added initial implementation for creating thread_pools

--- web_server/src/lib.rs
+++ web_server/src/lib.rs
@@ -1,4 +1,20 @@
-pub struct ThreadPool;
+use std::thread;
+
+/**
+ * Use of JoinHandler struct
+ * An owned permission to join on a thread (block on its termination). A JoinHandle detaches the associated thread when it is dropped, which means that there is no longer any handle to thread and no way to join on it.
+ */
+
+pub struct ThreadPool {
+  threads: Vec<thread::JoinHandle<()>>,
+}
+
+// this struct will store the id of the thread which will be doing the work and the thread that waits for listening and handling the requests
+
+pub struct Worker {
+  id: usize,
+  thread: thread::JoinHandle<()>,
+}
 
 impl ThreadPool {
   /**
@@ -12,7 +28,13 @@ impl ThreadPool {
    */
   pub fn new(size: usize) -> ThreadPool {
     assert!(size > 0);
-    ThreadPool
+
+    let mut threads = Vec::with_capacity(size);
+
+    for _ in 0..size {
+      // create threads
+    }
+    ThreadPool { threads }
   }
 
   pub fn execute<F>(&self, f: F)
