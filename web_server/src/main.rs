c8c29ad4b4234e02f024581cadd50e839e8f9fa6
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-30 14:35:05 +0530

    add: added handle connection method for buffer to string converter

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,7 +1,13 @@
-use std::io::prelude;
+use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
-fn handle_connection(mut stream: TcpStream) {}
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024]; // buffer to store the data from the stream
+
+    stream.read(&mut buffer).unwrap(); // sending the data from the stream to the buffer
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..])); // from_utf8_lossy is a method that converts a slice of bytes into string including invalid character
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
